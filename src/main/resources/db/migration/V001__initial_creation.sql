da76ef96cfc02143d2e3c218e59cda0ae3f98cc2
OsmarVitor <86305285+OsmarVitor@users.noreply.github.com>
2024-01-31 12:26:00 -0500

    Update V001__initial_creation.sql

--- src/main/resources/db/migration/V001__initial_creation.sql
+++ src/main/resources/db/migration/V001__initial_creation.sql
@@ -19,19 +19,11 @@ CREATE TABLE public.payment_type (
 );
 
 
-CREATE TABLE public."permission" (
-	id bigserial NOT NULL,
-	description varchar(50) NOT NULL,
-	"name" varchar(30) NOT NULL,
-	CONSTRAINT permission_pkey PRIMARY KEY (id)
-);
-
-
 CREATE TABLE public.state (
 	id bigserial NOT NULL,
 	"name" varchar(25) NOT NULL,
 	CONSTRAINT state_pkey PRIMARY KEY (id),
-	CONSTRAINT uk_2g0hi7w44i4sjkffh61pusaav UNIQUE (name)
+	CONSTRAINT state_name_unique UNIQUE (name)
 );
 
 
@@ -49,48 +41,86 @@ CREATE TABLE public.city (
 	"name" varchar(50) NOT NULL,
 	state_id int8 NOT NULL,
 	CONSTRAINT city_pkey PRIMARY KEY (id),
-	CONSTRAINT fk6p2u50v8fg2y0js6djc6xanit FOREIGN KEY (state_id) REFERENCES public.state(id)
+	CONSTRAINT fk_state_id FOREIGN KEY (state_id) REFERENCES public.state(id)
+);
+
+
+CREATE TABLE public."permission" (
+	id bigserial NOT NULL,
+	description varchar(50) NOT NULL,
+	"name" varchar(30) NOT NULL,
+	CONSTRAINT permission_pkey PRIMARY KEY (id)
 );
 
 
 CREATE TABLE public.group_permission (
 	groups_id int8 NOT NULL,
 	permission_id int8 NOT NULL,
-	CONSTRAINT fk8jfrgi2mrda23acrad626w74q FOREIGN KEY (groups_id) REFERENCES public."groups"(id),
-	CONSTRAINT fkss14p30qbokhpkpdov4ha3wro FOREIGN KEY (permission_id) REFERENCES public."permission"(id)
-);
-
-
-CREATE TABLE public.restaurant (
-	id bigserial NOT NULL,
-	complement varchar(30) NULL,
-	neighborhood varchar(30) NULL,
-	"number" varchar(10) NULL,
-	street varchar(50) NULL,
-	zip_code varchar(10) NULL,
-	delivery_fee numeric(19, 2) NOT NULL,
-	"name" varchar(50) NOT NULL,
-	address_city_id int8 NULL,
-	kitchen_id int8 NOT NULL,
-	CONSTRAINT restaurant_pkey PRIMARY KEY (id),
-	CONSTRAINT fk8pcwgn41lfg43d8u82ewn3cn FOREIGN KEY (address_city_id) REFERENCES public.city(id),
-	CONSTRAINT fkrur1dqx79jim8s8dvdp16qc3d FOREIGN KEY (kitchen_id) REFERENCES public.kitchen(id)
-);
-
-
-CREATE TABLE public.restaurant_payment_type (
-	restaurant_id int8 NOT NULL,
-	payment_type_id int8 NOT NULL,
-	CONSTRAINT fk36v769cr8ec61p38tcllgmiul FOREIGN KEY (payment_type_id) REFERENCES public.payment_type(id),
-	CONSTRAINT fkk8tfvxtsqptpfev7enmv0pby4 FOREIGN KEY (restaurant_id) REFERENCES public.restaurant(id)
+	CONSTRAINT fk_group_id FOREIGN KEY (groups_id) REFERENCES public."groups"(id),
+	CONSTRAINT fk_permission_id FOREIGN KEY (permission_id) REFERENCES public."permission"(id)
 );
 
 
 CREATE TABLE public.user_group (
 	users_id int8 NOT NULL,
 	group_id int8 NOT NULL,
-	CONSTRAINT fkbegtgnl3oq004958pisko4fu4 FOREIGN KEY (group_id) REFERENCES public."groups"(id),
-	CONSTRAINT fkg3bkro9p89yr5uestuf2n2vh9 FOREIGN KEY (users_id) REFERENCES public.users(id)
+	CONSTRAINT fk_group_id FOREIGN KEY (group_id) REFERENCES public."groups"(id),
+	CONSTRAINT fk_user_id FOREIGN KEY (users_id) REFERENCES public.users(id)
+);
+
+
+CREATE TABLE public.address (
+	id bigserial NOT NULL,
+	complement varchar(255) NULL,
+	neighborhood varchar(255) NULL,
+	"number" varchar(255) NULL,
+	street varchar(255) NULL,
+	zip_code varchar(255) NULL,
+	address_city_id int8 NULL,
+	CONSTRAINT address_pkey PRIMARY KEY (id),
+	CONSTRAINT fk_address_city_id FOREIGN KEY (address_city_id) REFERENCES public.city(id)
+);
+
+
+CREATE TABLE public.restaurant (
+	id bigserial NOT NULL,
+	delivery_fee numeric(19, 2) NOT NULL,
+	"name" varchar(50) NOT NULL,
+	address_id int8 NOT NULL,
+	kitchen_id int8 NOT NULL,
+	CONSTRAINT restaurant_pkey PRIMARY KEY (id),
+	CONSTRAINT fk_address_id FOREIGN KEY (address_id) REFERENCES public.address(id),
+	CONSTRAINT fk_kitchen_id FOREIGN KEY (kitchen_id) REFERENCES public.kitchen(id)
+);
+
+
+CREATE TABLE public.restaurant_payment_type (
+	restaurant_id int8 NOT NULL,
+	payment_type_id int8 NOT NULL,
+	CONSTRAINT fk_payment_type_id FOREIGN KEY (payment_type_id) REFERENCES public.payment_type(id),
+	CONSTRAINT fk_restaurant_id FOREIGN KEY (restaurant_id) REFERENCES public.restaurant(id)
+);
+
+
+CREATE TABLE public.orders (
+	id bigserial NOT NULL,
+	cancellation_date timestamp NULL,
+	confirmation_date timestamp NULL,
+	delivery_date timestamp NULL,
+	delivery_fee numeric(19, 2) NULL,
+	gross_amount numeric(19, 2) NULL,
+	net_amount numeric(19, 2) NULL,
+	order_status int4 NULL,
+	quantity int4 NULL,
+	address_id int8 NOT NULL,
+	payment_type_id int8 NOT NULL,
+	restaurant_id int8 NOT NULL,
+	user_id int8 NOT NULL,
+	CONSTRAINT orders_pkey PRIMARY KEY (id),
+	CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES public.users(id),
+	CONSTRAINT fk_payment_type_id FOREIGN KEY (payment_type_id) REFERENCES public.payment_type(id),
+	CONSTRAINT fk_address_id FOREIGN KEY (address_id) REFERENCES public.address(id),
+	CONSTRAINT fk_restaurant_id FOREIGN KEY (restaurant_id) REFERENCES public.restaurant(id)
 );
 
 
@@ -102,5 +132,19 @@ CREATE TABLE public.product (
 	price numeric(19, 2) NOT NULL,
 	restaurant_id int8 NOT NULL,
 	CONSTRAINT product_pkey PRIMARY KEY (id),
-	CONSTRAINT fkp4b7e36gck7975p51raud8juk FOREIGN KEY (restaurant_id) REFERENCES public.restaurant(id)
+	CONSTRAINT fk_restaurant_id FOREIGN KEY (restaurant_id) REFERENCES public.restaurant(id)
+);
+
+
+CREATE TABLE public.order_product (
+	id bigserial NOT NULL,
+	observation varchar(255) NULL,
+	quantity int4 NULL,
+	total_value numeric(19, 2) NULL,
+	unitary_value numeric(19, 2) NULL,
+	order_id int8 NOT NULL,
+	product_id int8 NOT NULL,
+	CONSTRAINT order_product_pkey PRIMARY KEY (id),
+	CONSTRAINT fk_order_product_id FOREIGN KEY (product_id) REFERENCES public.product(id),
+	CONSTRAINT fk_order_order_id FOREIGN KEY (order_id) REFERENCES public.orders(id)
 );
